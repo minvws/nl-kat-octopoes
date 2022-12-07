56f52b38f3cfe664799b1bd65501d7e5218845c5
ammar <ammar.abdulamir@gmail.com>
2022-12-07 13:39:51 +0100

    Updated `Certificate` model
    Signed-off-by: ammar <ammar.abdulamir@gmail.com>
    
    Fixed models
    
    Signed-off-by: ammar <ammar.abdulamir@gmail.com>
    
    Fixed `Certificate` model
    
    Signed-off-by: ammar <ammar.abdulamir@gmail.com>
    
    Fixed `Website.certificate`
    
    Signed-off-by: ammar <ammar.abdulamir@gmail.com>
    
    Added `Website.certificate` property
    
    Signed-off-by: ammar <ammar.abdulamir@gmail.com>
    
    Updated Certificate model
    
    Signed-off-by: ammar <ammar.abdulamir@gmail.com>
    
    Some updates
    
    Signed-off-by: ammar <ammar.abdulamir@gmail.com>
    
    Removed `Certificate.website` from natural keys
    
    Signed-off-by: ammar <ammar.abdulamir@gmail.com>
    
    Made `Certificate.website` optional
    
    Signed-off-by: ammar <ammar.abdulamir@gmail.com>
    
    Some updates
    
    Signed-off-by: ammar <ammar.abdulamir@gmail.com>
    
    Removed `Certificate.website` from natural keys
    
    Signed-off-by: ammar <ammar.abdulamir@gmail.com>
    
    Made `Certificate.website` optional
    
    Signed-off-by: ammar <ammar.abdulamir@gmail.com>

--- octopoes/models/ooi/certificate.py
+++ octopoes/models/ooi/certificate.py
@@ -2,11 +2,11 @@ from __future__ import annotations
 
 from datetime import datetime
 from enum import Enum
-from typing import Optional, Literal
+from typing import Optional, Literal, Union
 
 from octopoes.models import OOI, Reference
 from octopoes.models.ooi.dns.zone import Hostname
-from octopoes.models.ooi.web import Website
+from octopoes.models.ooi.network import IPAddress
 from octopoes.models.persistence import ReferenceField
 
 
@@ -19,34 +19,72 @@ class Certificate(OOI):
     object_type: Literal["Certificate"] = "Certificate"
 
     subject: str
-    issuer: str
+    issuer: Optional[str]
     valid_from: str
     valid_until: str
-    pk_algorithm: str
-    pk_size: int
-    pk_number: str
-
-    website: Reference = ReferenceField(Website, max_issue_scan_level=0, max_inherit_scan_level=1)
+    pk_algorithm: Optional[str]
+    pk_size: Optional[int]
+    pk_number: Optional[str]
     signed_by: Optional[Reference] = ReferenceField(
         "Certificate", max_issue_scan_level=1, max_inherit_scan_level=0, default=None
     )
+    serial_number: str
 
-    _natural_key_attrs = ["website", "subject", "issuer"]
+    _natural_key_attrs = ["issuer", "serial_number"]
 
     @property
     def expired(self):
         return datetime.now() > datetime.fromisoformat(self.valid_until)
 
     _reverse_relation_names = {
-        "website": "certificates",
         "signed_by": "signed_certificates",
     }
 
     @classmethod
     def format_reference_human_readable(cls, reference: Reference) -> str:
-        return f"{reference.tokenized.subject} ({reference.tokenized.issuer})"
+        return f"{reference.tokenized.issuer} ({reference.tokenized.serial_number})"
 
 
+class SubjectAlternativeName(OOI):
+    certificate: Reference = ReferenceField(Certificate)
+
+    _natural_key_attrs = ["certificate"]
+
+
+class SubjectAlternativeNameHostname(SubjectAlternativeName):
+    object_type: Literal["SubjectAlternativeNameHostname"] = "SubjectAlternativeNameHostname"
+    hostname: Reference = ReferenceField(Hostname, max_issue_scan_level=1, max_inherit_scan_level=0)
+
+    _natural_key_attrs = SubjectAlternativeName._natural_key_attrs + ["hostname"]
+
+    @classmethod
+    def format_reference_human_readable(cls, reference: Reference) -> str:
+        return reference.tokenized.hostname.name
+
+
+class SubjectAlternativeNameIP(SubjectAlternativeName):
+    object_type: Literal["SubjectAlternativeNameIP"] = "SubjectAlternativeNameIP"
+    address: Reference = ReferenceField(IPAddress)
+
+    _natural_key_attrs = SubjectAlternativeName._natural_key_attrs + ["address"]
+
+    @classmethod
+    def format_reference_human_readable(cls, reference: Reference) -> str:
+        return reference.tokenized.address.address
+
+
+class SubjectAlternativeNameQualifier(SubjectAlternativeName):
+    object_type: Literal["SubjectAlternativeNameQualifier"] = "SubjectAlternativeNameQualifier"
+    name: str
+
+    _natural_key_attrs = SubjectAlternativeName._natural_key_attrs + ["name"]
+
+    @classmethod
+    def format_reference_human_readable(cls, reference: Reference) -> str:
+        return reference.tokenized.name
+
+
+# todo: remove
 class CertificateSubjectAlternativeName(OOI):
     object_type: Literal["CertificateSubjectAlternativeName"] = "CertificateSubjectAlternativeName"
 
@@ -66,4 +104,7 @@ class CertificateSubjectAlternativeName(OOI):
 
 
 Certificate.update_forward_refs()
-CertificateSubjectAlternativeName.update_forward_refs()
+CertificateSubjectAlternativeName.update_forward_refs()  # todo: remove
+SubjectAlternativeNameHostname.update_forward_refs()
+SubjectAlternativeNameIP.update_forward_refs()
+SubjectAlternativeNameQualifier.update_forward_refs()
