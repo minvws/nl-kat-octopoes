421e669345d0c8cc5f8fde512bbac0660595b3a1
Patrick Darwinkel <patrick@code050.nl>
2023-02-09 13:43:36 +0100

    Rename schemavalidation function

--- tests/test_schema_validation.py
+++ tests/test_schema_validation.py
@@ -5,7 +5,7 @@ import pytest
 from octopoes.ddl.ddl import SchemaLoader, SchemaValidationException
 
 
-def schemavalidation(schema: str, expected_output: str) -> bool:
+def schema_validation(schema: str, expected_output: str) -> bool:
     """Helper function for testing schema validation."""
     with pytest.raises(SchemaValidationException) as exc:
         SchemaLoader((Path(__file__).parent / "fixtures" / schema).read_text())
@@ -21,80 +21,82 @@ def test_schema():
 
 
 def test_schema__natural_key_as_fields__schemavalidationerror():
-    assert schemavalidation(
+    assert schema_validation(
         "schema_incorrect_natural_key.graphql",
         "Natural keys must be defined as fields [type=Animal, natural_key=size]",
     )
 
 
 def test_schema__directive__schemavalidationerror():
-    assert schemavalidation(
+    assert schema_validation(
         "schema_directive.graphql",
         "A schema may only define a Type, Enum, Union, or Interface, not Directive [directive=test]",
     )
 
 
 def test_schema__constrained_types_no_scalar__schemavalidationerror():
-    assert schemavalidation(
+    assert schema_validation(
         "schema_custom_scalar.graphql",
         "A schema may only define a Type, Enum, Union, or Interface, not Scalar [type=CustomScalar]",
     )
 
 
 def test_schema__constrained_types_no_input__schemavalidationerror():
-    assert schemavalidation(
+    assert schema_validation(
         "schema_custom_input.graphql",
         "A schema may only define a Type, Enum, Union, or Interface, not Input [type=CatSpeech]",
     )
 
 
 def test_schema__reserved_type_name__schemavalidationerror():
-    assert schemavalidation(
+    assert schema_validation(
         "schema_reserved_type_name.graphql", "Use of reserved type name is not allowed [type=BaseObject]"
     )
 
 
 def test_schema__constrained_types_no_subscription__schemavalidationerror():
-    assert schemavalidation(
+    assert schema_validation(
         "schema_custom_subscription.graphql", "Use of reserved type name is not allowed [type=Subscription]"
     )
 
 
 def test_schema__constrained_types_no_query__schemavalidationerror():
-    assert schemavalidation("schema_custom_query.graphql", "Use of reserved type name is not allowed [type=Query]")
+    assert schema_validation("schema_custom_query.graphql", "Use of reserved type name is not allowed [type=Query]")
 
 
 def test_schema__constrained_types_no_mutation__schemavalidationerror():
-    assert schemavalidation(
+    assert schema_validation(
         "schema_custom_mutation.graphql", "Use of reserved type name is not allowed [type=Mutation]"
     )
 
 
 def test_schema__no_inherit_baseobject__schemavalidationerror():
-    assert schemavalidation(
+    assert schema_validation(
         "schema_no_baseobject_inheritance.graphql",
         "An object must inherit both BaseObject and OOI (missing BaseObject) [type=Test]",
     )
 
 
 def test_schema__no_inherit_ooi__schemavalidationerror():
-    assert schemavalidation(
+    assert schema_validation(
         "schema_no_ooi_inheritance.graphql", "An object must inherit both BaseObject and OOI (missing OOI) [type=Test]"
     )
 
 
 def test_schema__no_inherit_baseobject_ooi__schemavalidationerror():
-    assert schemavalidation(
+    assert schema_validation(
         "schema_no_baseobject_ooi_inheritance.graphql",
         "An object must inherit both BaseObject and OOI (missing both) [type=Test]",
     )
 
 
 def test_schema__no_union_with_u__schemavalidationerror():
-    assert schemavalidation("schema_no_union_with_u.graphql", "Self-defined unions must start with a U [type=Animals]")
+    assert schema_validation(
+        "schema_no_union_with_u.graphql", "Self-defined unions must start with a U [type=Animals]"
+    )
 
 
 def test_schema__pascalcased_names__schemavalidationerror():
-    assert schemavalidation(
+    assert schema_validation(
         "schema_no_pascalcase.graphql", "Object types must follow PascalCase conventions [type=zooKeeper]"
     )
